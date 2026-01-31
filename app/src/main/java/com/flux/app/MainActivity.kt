cd880333a143d0a85e144fee46559ba046cf1b03
sinanalizz123-max <sinanalizz123@gmail.com>
2026-01-31 15:43:39 +0530

    Fix: Define 'container' ID in activity_main.xml

--- app/src/main/java/com/flux/app/MainActivity.kt
+++ app/src/main/java/com/flux/app/MainActivity.kt
@@ -2,48 +2,10 @@ package com.flux.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
-import com.flux.app.ui.*
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
-
-        // Setup Swipe Adapter
-        viewPager.adapter = object : FragmentStateAdapter(this) {
-            override fun getItemCount(): Int = 4
-            override fun createFragment(position: Int): Fragment {
-                return when (position) {
-                    0 -> DashboardFragment()
-                    1 -> DataFragment()
-                    2 -> BatteryFragment()
-                    else -> SettingsFragment()
-                }
-            }
-        }
-
-        // Link Swipe to Bottom Nav
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                bottomNav.menu.getItem(position).isChecked = true
-            }
-        })
-
-        bottomNav.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_net -> viewPager.currentItem = 0
-                R.id.nav_data -> viewPager.currentItem = 1
-                R.id.nav_bat -> viewPager.currentItem = 2
-                R.id.nav_set -> viewPager.currentItem = 3
-            }
-            true
-        }
     }
 }
